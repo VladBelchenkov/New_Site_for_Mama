f13e690c016573628617b7ec6c3585a2c299bf11
VladBelchenkov <119514834+VladBelchenkov@users.noreply.github.com>
2023-04-03 17:53:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ $(window).scroll(function() {
     btn.removeClass('show');
   }
 }); 
+
+$('.carousel').on('touchstart', function(event){
+  const xClick = event.originalEvent.touches[0].pageX;
+  $(this).one('touchmove', function(event){
+      const xMove = event.originalEvent.touches[0].pageX;
+      const sensitivityInPx = 5;
+
+      if( Math.floor(xClick - xMove) > sensitivityInPx ){
+          $(this).carousel('next');
+      }
+      else if( Math.floor(xClick - xMove) < -sensitivityInPx ){
+          $(this).carousel('prev');
+      }
+  });
+  $(this).on('touchend', function(){
+      $(this).off('touchmove');
+  });
+});
